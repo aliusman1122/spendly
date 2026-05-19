8afdcde9feddbc70bce269c31c8a3edec7495ee5
[Mohammad <[khalilusman927@gmail.com]>
2026-05-19 14:52:35 +0500

    add coming soon

--- app.py
+++ app.py
@@ -217,6 +217,15 @@ def profile():
                          last_6m_from=last_6m_from.isoformat())
 
 
+@app.route("/analytics")
+def analytics():
+    # Redirect if not logged in
+    if not session.get("user_id"):
+        return redirect(url_for("login"))
+
+    return render_template("analytics.html")
+
+
 @app.route("/expenses/add")
 def add_expense():
     return "Add expense — coming in Step 7"
